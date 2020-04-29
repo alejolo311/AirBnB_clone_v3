70e20c4a3ef98bb44653b71ec6bf393c008baf6d
alejolo311 <alejolo311@gmail.com>
2020-04-29 10:48:55 -0500

    fix the call, the parametes is a classname not a str

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,25 +4,25 @@ index route for flask app
 """
 
 from models import storage
-from api.v1.views import app_views
+from api.v1.views import app_views, Amenity, City, Place, Review, State, User
 from flask import Flask, jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', strict_slashes=False, methods=['GET'])
 def status():
     """return the status"""
     return jsonify({"status": "OK"}), 200
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
 def stats():
     """send json with the count of objects"""
     counts = {
-        "amenities": storage.count('Amenity'),
-        "cities": storage.count('City'),
-        "places": storage.count('Place'),
-        "reviews": storage.count('Review'),
-        "states": storage.count('State'),
-        "users": storage.count('User')
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return jsonify(counts), 200
