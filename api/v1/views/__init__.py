7e338b16a29b047b50549b07b871c67a5874ea32
hb4y <hugobaymon@gmail.com>
2020-04-29 19:03:12 +0000

    [task 9] amenities methods

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -92,8 +92,8 @@ def put(model, m_id, ignore_keys):
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
-"""
 from api.v1.views.amenities import *
+"""
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
