68c7572692bf57e67fca6acd872ba2f8628e85aa
alejolo311 <alejolo311@gmail.com>
2020-04-29 18:21:10 -0500

    separate methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,21 +7,30 @@ from api.v1.views import *
 from flask import Flask, jsonify
 from models import storage
 from models.state import State
+from flasgger import swag_from
 
 
 @app_views.route("/states", strict_slashes=False, methods=["GET"])
-@app_views.route("/states/<state_id>", methods=["GET"])
+@swag_from('../doc/states/get.yml')
 def get_state(state_id=None):
+    """
+        GET Request for States
+    """
+    return jsonify([obj.to_dict() for obj in storage.all("State").values()])
+
+
+@app_views.route("/states/<state_id>", methods=["GET"])
+@swag_from('../doc/states/getById.yml')
+def get_state_by_id(state_id=None):
     """
         GET Request for States
     """
     if state_id:
         return get_model(State, state_id)
 
-    return jsonify([obj.to_dict() for obj in storage.all("State").values()])
-
 
 @app_views.route("/states/<state_id>", methods=["DELETE"])
+@swag_from('../doc/states/delete.yml')
 def delete_state(state_id):
     """
         DELETE Request for states
@@ -30,6 +39,7 @@ def delete_state(state_id):
 
 
 @app_views.route("/states", strict_slashes=False, methods=["POST"])
+@swag_from('../doc/states/post.yml')
 def post_state():
     """
         POST Request for States
@@ -38,6 +48,7 @@ def post_state():
 
 
 @app_views.route("/states/<state_id>", methods=["PUT"])
+@swag_from('../doc/states/put.yml')
 def put_state(state_id):
     """
         PUT Request for States
