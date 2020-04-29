d9879316786c69fd1e6c5b20c483bce6b611e940
hb4y <hugobaymon@gmail.com>
2020-04-29 05:02:22 +0000

    [task 7] Implement methods for States

---
+++ api/v1/views/states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+view for states
+"""
+
+from api.v1.views import *
+from flask import Flask, jsonify
+from models import storage
+from models.state import State
+
+
+@app_views.route("/states", strict_slashes=False, methods=["GET"])
+@app_views.route("/states/<state_id>", methods=["GET"])
+def get_state(state_id=None):
+    """
+        GET Request for States
+    """
+    if state_id:
+        return get_model("State", state_id)
+
+    return jsonify([obj.to_dict() for obj in storage.all("State").values()])
+
+
+@app_views.route("/states/<state_id>", methods=["DELETE"])
+def delete_state(state_id):
+    """
+        DELETE Request for states
+    """
+    return delete("State", state_id)
+
+
+@app_views.route("/states", strict_slashes=False, methods=["POST"])
+def post_state():
+    """
+        POST Request for States
+    """
+    return post("State", None, None, {"name"})
+
+
+@app_views.route("/states/<state_id>", methods=["PUT"])
+def put_state(state_id):
+    """
+        PUT Request for States
+    """
+    return put("State", state_id, ["id", "created_at", "updated_at"])
