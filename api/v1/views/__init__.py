188e2d60b77f59f70d1a65f7d56f66cd089397a2
hb4y <hugobaymon@gmail.com>
2020-04-29 05:01:48 +0000

    [task 7] Add all methods in a general way to apply to all task

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,100 @@
-
-from flask import Blueprint
+from flask import Blueprint, request, abort
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+from models.engine.db_storage import classes
+
+def parent_model(p_model, p_id, p_get):
+    """
+        GET Request
+    """
+    parent = storage.get(p_model, p_id)
+    if parent:
+        return jsonify([p.to_dict() for p in getattr(parent, p_get)]), 200
+    abort(404)
+
+
+def get_model(model, m_id):
+    """
+        GET Request
+    """
+    obj = storage.get(model, m_id)
+    if obj:
+        return jsonify(obj.to_dict()), 200
+    abort(404)
+
+
+def delete(model, m_id):
+    """
+        DELETE Request
+    """
+    obj = storage.get(model, m_id)
+    if obj:
+        storage.delete(obj)
+        storage.save()
+        return jsonify({}), 200
+    abort(404)
+
+
+def post(model, p_model, p_id, data):
+    """
+        POST Request
+    """
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({'error': 'Not a JSON'}), 400
+    
+    if p_model:
+        parent = storage.get(p_model, p_id)
+        if not parent:
+            abort(404)
+
+    for key in data:
+        if key not in json_data:
+            return jsonify({'error': 'Missing {}'.format(key)}), 400
+
+    if p_model:
+        aux_key = p_model.lower() + "_id"
+        json_data[aux_key] = p_id
+
+    obj = classes[model](**json_data)
+    obj.save()
+
+    return jsonify(obj.to_dict()), 201
+
+def put(model, m_id, ignore_keys):
+    """
+        PUT Request
+    """
+    json_data = request.get_json()
+
+    if not json_data:
+        return jsonify({'error': 'Not a JSON'}), 400
+
+    obj = storage.get(model, m_id)
+    if not obj:
+        abort(404)
+
+    for k, v in json_data.items():
+        if k not in ignore_keys:
+            setattr(obj, k, v)
+    obj.save()
+
+    return jsonify(obj.to_dict()), 200
+
 from api.v1.views.index import *
+from api.v1.views.states import *
+"""
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
+"""
